7b54e2d6812ac3fbc0532a770463deceed6c23f5
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-10 15:42:23 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,17 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+  validate :is_title_case
+
+
 
   def song_count
     self.songs.count
+  end
+
+  def is_title_case
+    if title.split.any?{|w|w[0].upcase != w[0]}
+      errors.add(:title, "Title must be in title case")
+    end
   end
 
   def make_title_case
